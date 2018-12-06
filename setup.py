62715704b7992bd54e82e8642766b113eb3d7bf2
Chris Lyver <clyver@makespace.com>
2018-12-05 22:38:12 -0500

    Initial hacking

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='DistLock',
+    version='0.1.0',
+    packages=['distlock'],
+    description='Distributed Locking Service',
+    url='https://github.com/makingspace/Distlock',
+    long_description='Distributed Locking Service',
+    install_requires=[
+        'consul_kv'
+    ],
+    tests_require=[
+        'mock',
+        'pytest',
+    ]
+)
