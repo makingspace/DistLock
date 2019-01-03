cae176f11573c4c13421ad8664b1ab9e38003860
Chris Lyver <clyver@makespace.com>
2019-01-03 17:21:35 -0500

    Dont destroy the session in acquire...that is what maintains the lock!

--- distlock/__init__.py
+++ distlock/__init__.py
@@ -4,18 +4,33 @@ import time
 GLOBAL_PREFIX = 'distlock/key'
 
 
-class DistLockConsulApp(object):
+class DistLockToken:
+    """
+    An object dispensed by a DistLockConsulInterface's acquire() method
+    that facilitates maintaining a lock-entry's state.
+    """
+    def __init__(self, key, acquired, session_id=None):
+        self.key = key
+        self.acquired = acquired
+        self.session_id = session_id
+
+
+class DistLockConsulInterface(object):
+
+    DEFAULT_SESSION_TTL = 30
+    DEFAULT_SESSION_LOCK_DELAY = 5
 
     def __init__(self, consul_host, consul_port):
         self.connection = consul.Consul(consul_host, consul_port, 'http')
 
+    def acquire_on_obj(self, obj, **kwargs):
+        key = self.get_key(obj)
+        return self.acquire(key, **kwargs)
+
     def acquire(self, key, session_id=None, wait_seconds=0):
 
-        if session_id:
-            session_created = False
-        else:
+        if not session_id:
             session_id = self._create_session()
-            session_created = True
 
         acquired = self.connection.kv.put(key=key, value=None, acquire=session_id)
 
@@ -28,11 +43,7 @@ class DistLockConsulApp(object):
                 else:
                     time.sleep(float(1/3))
 
-        if session_created:
-            # Clean up the session we made.
-            self._destroy_session(session_id)
-
-        return (True, session_id) if acquired else (False, None)
+        return DistLockToken(key=key, acquired=acquired, session_id=session_id)
 
     def release(self, key, session_id, destroy_session=True):
 
@@ -63,6 +74,13 @@ class DistLockConsulApp(object):
         """
         if session_params is None:
             session_params = {}
+
+        # Warning: Consul only ensures a session isn't invalidated _before_ the TTL is reached.
+        # The session's actual duration may be up to ~2x the specified TTL duration!
+        # See GitHub issue: https://github.com/hashicorp/consul/issues/2966
+        session_params.setdefault('ttl', self.DEFAULT_SESSION_TTL)
+        session_params.setdefault('lock_delay', self.DEFAULT_SESSION_LOCK_DELAY)
+
         return self.connection.session.create(**session_params)
 
     def _destroy_session(self, session_id):
