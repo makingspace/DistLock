67d3e60624d681f09d13413e95f5b5be46bd48ac
Chris Lyver <clyver@makespace.com>
2019-01-18 10:48:05 -0500

    include python-consul in install requires

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     url='https://github.com/makingspace/Distlock',
     long_description='Distributed Locking Service',
     install_requires=[
-        'consul_kv'
+        'consul_kv',
+        'python-consul==1.0.1'
     ],
     tests_require=[
-        'python-consul',
+        'python-consul==1.0.1',
         'mock',
         'pytest',
     ]
